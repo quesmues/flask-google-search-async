99198cf83c82d74e3ce2468c424e1204f53eeecf
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-04-26 11:03:25 -0300

    tests: criado 2 testes, um para integração da rota e outro unitário para função async.

---
+++ flask_api/tests/test_search.py
@@ -0,0 +1,30 @@
+import pytest
+
+from flask_api.app import app
+from flask_api.main.routes import get_search_data
+
+
+@pytest.fixture(scope='module')
+def test_client():
+    with app.test_client() as testing_client:
+        yield testing_client
+
+
+def test_get_scrap_google_search(test_client):
+    """
+    Teste da rota "/api/v1/scrap-google-search"
+    """
+    response = test_client.get('/api/v1/scrap-google-search?search=teste')
+    assert response.status_code == 200
+    assert b"links" in response.data and b"search_text" in response.data
+
+
+@pytest.mark.asyncio
+async def test_async_get_search_data():
+    """
+    Teste unitário da função "get_search_data"
+    """
+    data = await get_search_data("teste")
+    assert isinstance(data, dict)
+    assert data.get("search_text", None)
+    assert data.get("links", None)
