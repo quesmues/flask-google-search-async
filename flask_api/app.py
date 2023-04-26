91ab4cd49acffdbf5b0474e2030d8659f93532e9
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-04-26 11:04:09 -0300

    fix: adicionado fix para rodar os tests no windows e não retornar erro de "Event Loop".

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,8 +1,14 @@
+import asyncio
+import os
+
 from asgiref.wsgi import WsgiToAsgi
 from flask import Flask
 
 from flask_api.config import Config
 from flask_api.main.routes import scrap_google
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 app = Flask(__name__)
 app.config.from_object(Config)
