0b22c9d5cab1b38fe0edb62a571fa11efa102967
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-04-26 00:12:15 -0300

    feat: endpoint de pesquisa e métrica criados.

--- flask-api/config.py
+++ flask-api/config.py
@@ -4,5 +4,15 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    """
+    Arquivo de configuração do Flask
+    """
+
     SECRET_KEY = "28465d8dfeefa59211639cacd4b5cdc6"
     GOOGLE_URL = "https://www.google.com"
+    BROWSER_HEADER = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9,lt;q=0.8,et;q=0.7,de;q=0.6",
+    }
--- flask-api/main/routes.py
+++ flask-api/main/routes.py
@@ -1,26 +1,72 @@
-import asyncio
+import time
+from urllib.parse import quote
 
-from flask import Blueprint
+import aiohttp
+from flask import Blueprint, current_app
 from flask import request
 from flask.views import View
+from bs4 import BeautifulSoup
 
 scrap_google = Blueprint('scrap_google', __name__)
 
+metrics = []
 
-async def async_get_data(search: str):
-    await asyncio.sleep(1)
-    return search
+
+async def get_search_data(search: str):
+    """
+    Função assíncrona que retorna os 5 primeiros links da pesquisa do Google
+
+    :param search: Texto a ser buscado na pesquisa do Google
+    :return: Uma lista com os primeiros 5 links da pesquisa do Google
+    """
+    global metrics
+    url = current_app.config['GOOGLE_URL'] + f"/search?q={quote(search)}"
+    headers = current_app.config['BROWSER_HEADER']
+    start = time.time()
+    links = []
+    async with aiohttp.ClientSession(headers=headers) as session:
+        response = await session.get(url, allow_redirects=True, timeout=30)
+        body = await response.text()
+        soup = BeautifulSoup(body, 'html.parser')
+        for result in soup.select('.tF2Cxc', limit=5):
+            links.append(result.select_one('.yuRUbf a')['href'])
+    end = time.time()
+    data = {
+        "search_text": search,
+        "response_time": end - start
+    }
+    metrics.append(data)
+    return links
 
 
 class ScrapGoogleSearchView(View):
+    """
+    View que faz uma busca no Google de forma assíncrona,
+    e retorna os 5 primeiros links que o Google respondeu
+    """
     async def dispatch_request(self):
         args = request.args.to_dict()
-        assert args.get("search")
-        data = await async_get_data(args.get("search"))
-        return data
+        search = args.get("search", None)
+        if not search:
+            return {"error": "Parâmetros obrigatórios não informados!"}
+        done = await get_search_data(search)
+        return done
+
+
+class GetMetricsView(View):
+    """
+    Metricas das requisições da view `ScrapGoogleSearchView`
+    """
+    async def dispatch_request(self):
+        global metrics
+        return metrics
 
 
 scrap_google.add_url_rule(
     "/scrap-google-search",
     view_func=ScrapGoogleSearchView.as_view("scrap_list")
+)
+scrap_google.add_url_rule(
+    "/metrics",
+    view_func=GetMetricsView.as_view("metrics_list")
 )
