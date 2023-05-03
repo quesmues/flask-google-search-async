a8ef06e2e9949d7aac4d717eb1f7e929a8a5449d
Eduardo Czamanski Rota <eduardoczamanski@protonmail.com>
2023-05-03 18:40:35 -0300

    tests: alterado para usar apenas o asyncio para teste de performance.

--- flask_api/tests/test_performance.py
+++ flask_api/tests/test_performance.py
@@ -26,29 +26,15 @@ async def client_task() -> None:
         _ = await session.get(URL.format(random.choice(WORDS)))
 
 
-async def stress_http_connection():
-    tasks = [asyncio.create_task(client_task())]
+async def stress_http_connection(qtd: int) -> None:
+    tasks = [asyncio.create_task(client_task()) for _ in range(qtd)]
     _ = await asyncio.gather(*tasks)
-
-
-def async_wrapper():
-    asyncio.run(stress_http_connection())
 
 
 if "__main__" == __name__:
     qtd = 10
     processes = []
     start = time.time()
-    for _ in range(qtd):
-        process = Process(target=async_wrapper)
-        process.daemon = True
-        process.start()
-        processes.append(process)
-
-    for process in processes:
-        process.join()
-    for process in processes:
-        process.terminate()
-
+    asyncio.run(stress_http_connection(qtd))
     end = time.time()
     print(f"{qtd} de requisições realizada em {end-start:.2f} segundos!")
